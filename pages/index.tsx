e2e4284c730cb5393265190ff5e7888129065f80
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-09-21 20:56:50 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import BiolinkCard from '../components/BiolinkCard'
 export default function Home() {
   return (
     <main>
-      <BiolinkCard />
+      <BiolinkCard profileImageUrl="https://preview.redd.it/rtxckdqn4qk71.jpg?width=640&crop=smart&auto=webp&s=361061df2f2be4434d317141dfcecd7805cf7a88" />
     </main>
   )
 }
