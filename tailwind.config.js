028b0a231d3a092f242845ff84cdccb1ea3b3e28
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-09-21 20:49:16 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        gradient: 'gradient 3s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
